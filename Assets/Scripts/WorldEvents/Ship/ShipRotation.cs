e5c16c77b4b57e070068c2d00973c72f072d4217
Lucas Hilgevoord <lucashilgevoordd@gmail.com>
2019-02-03 19:54:47 +0100

    Furiture models, Fog, Ship rotation

---
+++ Assets/Scripts/WorldEvents/Ship/ShipRotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipRotation : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine("ObjectRotate");
+    }
+
+    IEnumerator ObjectRotate()
+    {
+        float timer = 10000;
+        while (true)
+        {
+            float angle = Mathf.Sin(timer) * 3.7f;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
