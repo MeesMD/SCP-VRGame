5ecff49775f675ec1996c2913ac55c9e4e89dfde
FloydMa1 <f.lehman@ziggo.nl>
2019-02-04 11:55:39 +0100

    Walking with collision

--- Assets/Scripts/VR Scripts/InputHandler.cs
+++ Assets/Scripts/VR Scripts/InputHandler.cs
@@ -6,17 +6,22 @@ using Valve.VR;
 public class InputHandler : MonoBehaviour
 {
     [SerializeField] private SteamVR_Action_Vector2 touchpad;
-    [SerializeField] private SteamVR_Action_Boolean touchpadPress;
+    [SerializeField] private SteamVR_Action_Boolean touchpadPressL;
+    [SerializeField] private SteamVR_Action_Boolean touchpadPressR;
 
-    [SerializeField] private GameObject controller;
+    [SerializeField] private GameObject controllerL;
     [SerializeField] private GameObject player;
 
-    private Vector2 touchpadValue;
-    private double walkMaxTPValue = 0.7f;
-    private double walkMinTPValue = 0.7f;
-    private bool touchpadPressed = false;
+    private Vector2 touchpadValueL;
+    private bool touchpadPressedL = false;
+    private float walkVelocity = 1;
+    private float runVelocity = 2.3f;
 
-    private float velocity = 2;
+    private Vector2 touchpadValueR;
+    private Quaternion currentRotation;
+    private bool touchpadPressedR = false;
+    private float tpDir = 0.7f;
+
 
     private void Start()
     {
@@ -25,24 +30,63 @@ public class InputHandler : MonoBehaviour
 
     private void Update()
     {
-        touchpadValue = touchpad.GetAxis(SteamVR_Input_Sources.Any);
-        touchpadPressed = touchpadPress.GetState(SteamVR_Input_Sources.Any);
+        touchpadValueL = touchpad.GetAxis(SteamVR_Input_Sources.LeftHand);
+        touchpadPressedL = touchpadPressL.GetState(SteamVR_Input_Sources.LeftHand);
 
-        if (touchpadValue.y > walkMaxTPValue && touchpadPressed == true) 
+        touchpadValueR = touchpad.GetAxis(SteamVR_Input_Sources.RightHand);
+        touchpadPressedR = touchpadPressR.GetState(SteamVR_Input_Sources.RightHand);
+
+        
+    }
+
+    private void Rotate()
+    {
+        currentRotation = player.transform.rotation;
+        
+        if(touchpadValueR.y < -tpDir && touchpadPressedR)
         {
-            Move();
+            //Down
+            player.transform.Rotate(0, 180, 0);
+        }
+
+        if (touchpadValueR.x > tpDir && touchpadPressedR)
+        {
+            //Right
+        }
+        else if (touchpadValueR.x < -tpDir && touchpadPressedR)
+        {
+            //Left
+        }
+    }
+
+    private void CheckMove()
+    {
+        if (touchpadValueL.y > tpDir && touchpadPressedL == false)
+        {
+            Walk();
+        }
+        else if (touchpadValueL.y > tpDir && touchpadPressedL == true)
+        {
+            Run();
         }
         else
         {
-            touchpadPressed = false;
+            touchpadPressedL = false;
             return;
         }
     }
 
-    private void Move()
+    private void Walk()
     {
         Debug.Log("Walk");
-        player.GetComponent<Rigidbody>().velocity = controller.transform.forward * velocity;
+        player.GetComponent<Rigidbody>().velocity = controllerL.transform.forward * walkVelocity;
+        return;
+    }
+
+    private void Run()
+    {
+        Debug.Log("Run");
+        player.GetComponent<Rigidbody>().velocity = controllerL.transform.forward * runVelocity;
         return;
     }
 
