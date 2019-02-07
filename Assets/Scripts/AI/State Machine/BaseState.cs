3f488b4b923a9bfeeef8ed8bc1f3faa834a4da81
Antoine Botteler <antonio3338@gmail.com>
2019-02-07 12:29:10 +0100

    State Machine, C# 7.0, audio spatializer plugin added.

---
+++ Assets/Scripts/AI/State Machine/BaseState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace qt.AI
+{
+    public abstract class BaseState : MonoBehaviour
+    {
+        public StateMachine StateMachine { get; set; }
+        
+        public abstract void EnterState();
+        public abstract void ExitState();
+        public abstract void Act();
+        public abstract void Think();
+    }
+}
---
+++ Assets/Scripts/AI/State Machine/StateMachine.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace qt.AI
+{
+    public class StateMachine : MonoBehaviour
+    {
+        private Dictionary<string, BaseState> _states;
+
+        private BaseState _currentState;
+        
+        public BaseState CurrentState { get; private set; }
+
+        void Start()
+        {
+            GetStates();
+        }
+
+        public void SetState(string stateName)
+        {
+            if (!_states.TryGetValue(stateName.ToLower(), out var state))
+            {
+                Debug.LogError($@"State ""{stateName}"" not found.");
+                return;
+            }
+            
+            if(_currentState != null)
+                _currentState.ExitState();
+
+            _currentState = state;
+            _currentState.EnterState();
+        }
+
+        private void GetStates()
+        {
+            // get all states of the current object.
+            _states = new Dictionary<string, BaseState>();
+            var states = gameObject.GetComponents<BaseState>();
+
+            foreach (var state in states)
+            {
+                state.StateMachine = this;
+                _states.Add(states.GetType().Name.ToLower(), state);
+            }
+        }
+
+        private void Update()
+        {
+            if (_currentState == null) return;
+            _currentState.Think();
+            _currentState.Act();
+        }
+    }
+}
